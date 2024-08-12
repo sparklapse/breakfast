e93103ae0c22a7f4dc9adbf07e1c16fde5b0a53e
Mr Josh <dotmrjosh@sparklapse.com>
2024-08-12 04:12:39 +0000

    Style fix for sign in page

--- www/src/routes/index.tsx
+++ www/src/routes/index.tsx
@@ -48,7 +48,7 @@ function LoginScreen() {
                     type="text"
                     autocomplete="username"
                     required
-                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-slate-600 sm:text-sm sm:leading-6"
+                    class="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-slate-600 sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
@@ -64,7 +64,7 @@ function LoginScreen() {
                     type="password"
                     autocomplete="current-password"
                     required
-                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-slate-600 sm:text-sm sm:leading-6"
+                    class="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-slate-600 sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
