3f9a202e82822bc41383c66b59acef8798cb6375
Mr Josh <dotmrjosh@sparklapse.com>
2024-08-20 12:30:54 +0000

    Added some eventsub subscriptions

--- services/events/twitch/eventsub/subscriptions.go
+++ services/events/twitch/eventsub/subscriptions.go
@@ -6,6 +6,29 @@ type SubscriptionConfig struct {
 	Condition map[string]string
 }
 
+// Broadcaster goes live
+func CreateStreamOnlineSubscription(twitch_broadcaster_id string) SubscriptionConfig {
+	return SubscriptionConfig{
+		Type:    "stream.online",
+		Version: "1",
+		Condition: map[string]string{
+			"broadcaster_user_id": twitch_broadcaster_id,
+		},
+	}
+}
+
+// Broadcaster goes offline
+func CreateStreamOfflineSubscription(twitch_broadcaster_id string) SubscriptionConfig {
+	return SubscriptionConfig{
+		Type:    "stream.offline",
+		Version: "1",
+		Condition: map[string]string{
+			"broadcaster_user_id": twitch_broadcaster_id,
+		},
+	}
+}
+
+// Message sent in chat
 func CreateChannelChatMessageSubscription(twitch_broadcaster_id string, twitch_user_id string) SubscriptionConfig {
 	return SubscriptionConfig{
 		Type:    "channel.chat.message",
@@ -17,6 +40,31 @@ func CreateChannelChatMessageSubscription(twitch_broadcaster_id string, twitch_u
 	}
 }
 
+// Broadcaster shouts someone out
+func CreateShoutoutCreateSubscription(twitch_broadcaster_id string, moderator_twitch_id string) SubscriptionConfig {
+	return SubscriptionConfig{
+		Type:    "channel.shoutout.create",
+		Version: "1",
+		Condition: map[string]string{
+			"broadcaster_user_id": twitch_broadcaster_id,
+			"moderator_user_id":   moderator_twitch_id,
+		},
+	}
+}
+
+// Broadcaster receives a shoutout
+func CreateShoutoutReceiveSubscription(twitch_broadcaster_id string, moderator_twitch_id string) SubscriptionConfig {
+	return SubscriptionConfig{
+		Type:    "channel.shoutout.receive",
+		Version: "1",
+		Condition: map[string]string{
+			"broadcaster_user_id": twitch_broadcaster_id,
+			"moderator_user_id":   moderator_twitch_id,
+		},
+	}
+}
+
+// Create an array events for most common things users would listen to
 func CreateDefaultSubscriptions(twitch_user_id string) []SubscriptionConfig {
 	return []SubscriptionConfig{
 		CreateChannelChatMessageSubscription(twitch_user_id, twitch_user_id),
