e377b42535c9ef1d00b947c542c973ec5c6e4d40
Mr Josh <dotmrjosh@sparklapse.com>
2024-08-22 12:26:23 +0000

    Added twitch account linking on account page

--- admin/src/routes/(dashboard)/account/+page.svelte
+++ admin/src/routes/(dashboard)/account/+page.svelte
@@ -2,7 +2,7 @@
   import clsx from "clsx";
   import toast from "svelte-french-toast";
   import { Eye, EyeOff } from "lucide-svelte";
-  import { invalidateAll } from "$app/navigation";
+  import { invalidate, invalidateAll } from "$app/navigation";
 
   import type { PageData } from "./$types";
   export let data: PageData;
@@ -67,6 +67,50 @@
           </button>
         </div>
       </div>
+      {#await data.suspense.identities}
+        <p>Loading</p>
+      {:then identities}
+        <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+          <dt class="text-sm leading-6 text-gray-900">Twitch Account</dt>
+          <div
+            class="mt-1 flex items-center justify-between text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"
+          >
+            <dd>
+              {identities.twitch ? identities.twitch.providerId : "Not Linked"}
+            </dd>
+            <button
+              class="rounded bg-purple-900 px-4 text-white"
+              on:click={() => {
+                if (identities.twitch) {
+                  if (!$user) return;
+                  toast.promise(
+                    data.pb.collection("users").unlinkExternalAuth($user.id, "twitch"),
+                    {
+                      loading: "Unlinking account...",
+                      success: "Unlinked twitch account!",
+                      error: (err) => `Failed to unlink twitch account: ${err.message}`,
+                    },
+                  );
+                  return;
+                }
+
+                toast.promise(
+                  data.pb.breakfast.auth.sso.twitch().then(() => {
+                    invalidate("pb:account");
+                  }),
+                  {
+                    loading: "Linking twitch account",
+                    success: "Twitch account linked!",
+                    error: (err) => `Failed to link twitch: ${err.message}`,
+                  },
+                );
+              }}
+            >
+              {identities.twitch ? "Unlink" : "Link"}
+            </button>
+          </div>
+        </div>
+      {/await}
     </dl>
   </div>
 </div>
