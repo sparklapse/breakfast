cfe1bb20f95380c3fa9949ec186751145ba2446b
Mr Josh <dotmrjosh@sparklapse.com>
2024-08-30 14:27:31 +0000

    Removed unused component for now

--- www/admin/src/lib/components/event-displays/index.ts
+++
@@ -1,6 +0,0 @@
-import ChatMessage from "./ChatMessage.svelte";
-import type { ComponentType } from "svelte";
-
-export const DISPLAYS: Record<string, ComponentType> = {
-  "chat-message": ChatMessage,
-};
