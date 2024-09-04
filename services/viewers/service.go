9192663b7ceb368e480981a999d1658d7d7f9755
Mr Josh <dotmrjosh@sparklapse.com>
2024-09-04 16:59:21 +0000

    Added more viewer stat numbers

--- services/viewers/service.go
+++ services/viewers/service.go
@@ -1,7 +1,10 @@
 package viewers
 
 import (
+	"time"
+
 	"github.com/labstack/echo/v5"
+	"github.com/pocketbase/dbx"
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/core"
 )
@@ -13,18 +16,38 @@ func RegisterService(app *pocketbase.PocketBase) {
 
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
 		e.Router.GET("/api/breakfast/viewers/count", func(c echo.Context) error {
+			response := map[string]int{}
+
 			var query struct {
 				Count int `db:"count"`
 			}
-			err := app.Dao().DB().
-				Select("COUNT(*) as count").
-				From("viewers").
-				One(&query)
-			if err != nil {
-				return c.JSON(500, map[string]string{"message": "Failed to count viewers"})
+
+			{
+				err := app.Dao().DB().
+					Select("COUNT(*) as count").
+					From("viewers").
+					One(&query)
+				if err != nil {
+					return c.JSON(500, map[string]string{"message": "Failed to count viewers", "error": err.Error()})
+				}
 			}
 
-			return c.JSON(200, map[string]int{"count": query.Count})
+			response["total"] = query.Count
+
+			{
+				err := app.Dao().DB().
+					Select("COUNT(*) as count").
+					From("viewers").
+					Where(dbx.NewExp("created > {:timeFrame}", dbx.Params{"timeFrame": time.Now().Add(30 * 24 * time.Hour * -1)})).
+					One(&query)
+				if err != nil {
+					return c.JSON(500, map[string]string{"message": "Failed to count new viewers", "error": err.Error()})
+				}
+			}
+
+			response["new30"] = query.Count
+
+			return c.JSON(200, response)
 		})
 
 		return nil
