eda6ce0aaab8b96817dc31722fbd7e53a6d76e41
Mr Josh <dotmrjosh@sparklapse.com>
2024-09-04 18:30:51 +0000

    Updated basic chat with new types

--- www/admin/src/lib/overlay/scripts/basics/chat.svelte
+++ www/admin/src/lib/overlay/scripts/basics/chat.svelte
@@ -7,17 +7,30 @@
 
 <script lang="ts">
   import { onMount } from "svelte";
-  import type { ChatEvent } from "@sparklapse/breakfast/overlay";
   import { fade, fly } from "svelte/transition";
+  import type { ChatMessageEvent } from "@sparklapse/breakfast/overlay";
+  import clsx from "clsx";
 
   export let removeTime: string = "3000";
 
   $$restProps;
 
-  let messages: { m: ChatEvent[] } = { m: [] };
+  let messages: { m: ChatMessageEvent[] } = { m: [] };
 
   onMount(() => {
     const unlisten = window.breakfast.events.listen((ev) => {
+      if (ev.type === "chat-message-delete") {
+        const { m } = messages;
+        const idx = m.findIndex((msg) => msg.data.id === ev.data.id);
+        if (idx !== -1) {
+          m[idx] = {
+            ...m[idx],
+            deleted: true,
+          };
+        }
+        messages = { m };
+        return;
+      }
       if (ev.type !== "chat-message") return;
 
       const { m } = messages;
@@ -43,8 +56,12 @@
 
 <div class="container">
   {#each messages.m as msg (msg.data.id)}
-    <p in:fly={{ x: -40, duration: 100 }} out:fade={{ duration: 2000 }}>
-      <span style:color={msg.data.color}>{msg.data.chatter.displayName}</span>: {msg.data.text}
+    <p
+      class={clsx([msg.deleted && "hidden"])}
+      in:fly={{ x: -40, duration: 100 }}
+      out:fade={{ duration: 2000 }}
+    >
+      <span style:color={msg.data.color}>{msg.data.viewer.displayName}</span>: {msg.data.text}
     </p>
   {/each}
 </div>
