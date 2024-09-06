dacba5ee67cbd547a8b69833b9a3408f62153652
Mr Josh <dotmrjosh@sparklapse.com>
2024-09-06 23:55:06 +0000

    Fixed stream online event processing

--- services/events/twitch/eventsub/subscriptions/stream_online.go
+++ services/events/twitch/eventsub/subscriptions/stream_online.go
@@ -18,17 +18,22 @@ func CreateStreamOnlineSubscription(twitch_broadcaster_id string) SubscriptionCo
 }
 
 func ProcessStreamOnlinePayload(payload map[string]any) (*types.StreamOnline, error) {
+	event, ok := payload["event"].(map[string]any)
+	if !ok {
+		return nil, errors.New("event field was not of the correct type")
+	}
+
 	// broadcaster_user_id, valid := payload["broadcaster_user_id"].(string)
 	// if !valid {
 	// 	return nil, errors.New("broadcaster_user_id field was not of the correct type")
 	// }
 
-	broadcaster_user_login, valid := payload["broadcaster_user_login"].(string)
+	broadcaster_user_login, valid := event["broadcaster_user_login"].(string)
 	if !valid {
 		return nil, errors.New("broadcaster_user_login field was not of the correct type")
 	}
 
-	broadcaster_user_name, valid := payload["broadcaster_user_name"].(string)
+	broadcaster_user_name, valid := event["broadcaster_user_name"].(string)
 	if !valid {
 		return nil, errors.New("broadcaster_user_name field was not of the correct type")
 	}
