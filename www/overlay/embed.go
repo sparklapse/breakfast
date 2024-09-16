bde373fe51539b923da89518634e7683491b2f04
mr-josh <mr-josh@sparklapse.com>
2024-09-17 01:21:20 +1000

    Switched overlay back to sse only
    Will create a seperate local only project due to weird vite things

--- www/overlay/embed.go
+++ www/overlay/embed.go
@@ -6,18 +6,12 @@ import (
 	"text/template"
 )
 
-//go:embed dist/sse.html
+//go:embed dist/index.html
 var sseTemplateSource string
-
-//go:embed dist/local.html
-var localTemplateSource string
-
 var OverlayTemplate *template.Template
-var LocalOverlayTemplate *template.Template
 
 func init() {
 	OverlayTemplate = template.Must(template.New("overlay").Parse(sseTemplateSource))
-	LocalOverlayTemplate = template.Must(template.New("overlay-local").Parse(localTemplateSource))
 }
 
 type OverlayTemplateParams struct {
--- www/overlay/vite.config.ts
+++ www/overlay/vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import { viteSingleFile } from "vite-plugin-singlefile";
-import { resolve } from "node:path";
 
 export default defineConfig({
   plugins: [
-    viteSingleFile({ removeViteModuleLoader: true, useRecommendedBuildConfig: false }),
+    viteSingleFile({ removeViteModuleLoader: true }),
     {
       name: "de-module",
       transformIndexHtml: (html) => {
@@ -12,15 +11,4 @@ export default defineConfig({
       },
     },
   ],
-  build: {
-    assetsInlineLimit: Infinity,
-    chunkSizeWarningLimit: Infinity,
-    cssCodeSplit: false,
-    rollupOptions: {
-      input: {
-        sse: resolve("sse.html"),
-        local: resolve("local.html"),
-      },
-    },
-  },
 });
