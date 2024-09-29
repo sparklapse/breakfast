34f4ef336c5d9570482b9c8d315d6c335afbc52c
mr-josh <mr-josh@sparklapse.com>
2024-09-30 02:32:53 +1000

    Updated pages render to render data
    If data and translations are available, then will parse and execute the
    template with said data.

--- services/pages/service.go
+++ services/pages/service.go
@@ -1,6 +1,9 @@
 package pages
 
 import (
+	"html/template"
+	"strings"
+
 	"github.com/labstack/echo/v5"
 	"github.com/pocketbase/dbx"
 	"github.com/pocketbase/pocketbase"
@@ -40,7 +43,93 @@ func RegisterService(app *pocketbase.PocketBase) {
 
 			record := records[0]
 			html := record.GetString("html")
-			return c.HTML(200, html)
+
+			if record.GetString("data") == "null" {
+				return c.HTML(200, html)
+			}
+
+			var translations []map[string]any
+			{
+				err := record.UnmarshalJSONField("data", &translations)
+				if err != nil {
+					app.Logger().Error(
+						"PAGES Failed to parse page data",
+						"error", err.Error(),
+					)
+
+					// Render page as html to try and recover anyways
+					return c.HTML(200, html)
+				}
+			}
+
+			if len(translations) == 0 {
+				app.Logger().Warn(
+					"PAGES Translations was empty",
+					"pageId", record.Id,
+					"path", record.GetString("path"),
+				)
+
+				return c.HTML(200, html)
+			}
+
+			tmpl, err := template.New(record.Id).Parse(html)
+
+			requestedLang := c.QueryParam("lang")
+			accepted := strings.Split(c.Request().Header.Get("Accept-Language"), ";")
+			for _, translation := range translations {
+				lang, ok := translation["lang"].(string)
+				if !ok {
+					app.Logger().Warn(
+						"PAGES Translation didn't contain a lang field",
+						"pageId", record.Id,
+						"path", record.GetString("path"),
+					)
+					continue
+				}
+
+				if requestedLang != "" {
+					if lang == requestedLang {
+						err := tmpl.Execute(c.Response().Writer, translation)
+						if err != nil {
+							app.Logger().Error(
+								"PAGES Failed to parse page template",
+								"error", err.Error(),
+							)
+							return nil
+						}
+						return nil
+					}
+					continue
+				}
+
+				for _, accept := range accepted {
+					sections := strings.Split(accept, ",")
+					for _, section := range sections {
+						if strings.HasPrefix(section, lang) {
+							err := tmpl.Execute(c.Response().Writer, translation)
+							if err != nil {
+								app.Logger().Error(
+									"PAGES Failed to parse page template",
+									"error", err.Error(),
+								)
+								return nil
+							}
+							return nil
+						}
+					}
+				}
+			}
+
+			// Couldn't find a suitable translation so default to first available
+			data := translations[0]
+			{
+				c.Response().WriteHeader(200)
+				err := tmpl.Execute(c.Response().Writer, data)
+				if err != nil {
+					return nil
+				}
+				return nil
+			}
 		})
 
 		return nil
